9b7e1ac3d21f8400dca42af77d31d6afc7887a84
srthapa <srthapa927@gmail.com>
2019-06-28 16:28:30 +0545

    folder added

---
+++ src/components/leftSidebar/LeftSidebar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { withRouter, Link } from "react-router-dom";
+
+const sideNavRoutes = [
+  {
+    to: `/project-settings`,
+    path: "/project-settings",
+    title: "Project Information"
+  },
+  { to: "/site-type", path: "/site-type", title: "Site Types" },
+  {
+    to: "/site-information",
+    path: "/site-information",
+    title: "Site Information"
+  },
+  { to: "/manage-region", path: "/manage-region", title: "Manage Region" },
+  { to: "/map-layer", path: "/map-layer", title: "Map Layers" },
+  { to: "/term-and-label", path: "/term-and-label", title: "Terms And Labels" }
+];
+
+class LeftSidebar extends Component {
+  render() {
+    const {
+      location: { pathname }
+    } = this.props;
+    return (
+      <ul className="nav nav-tabs flex-column border-tabs">
+        {sideNavRoutes.map((route, i) => (
+          <li className="nav-item" key={i}>
+            <Link
+              to={route.to}
+              className={
+                pathname.includes(route.path) ? "nav-link active" : "nav-link"
+              }
+            >
+              {route.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default withRouter(LeftSidebar);
