bf26bbc8b0366cd4b4b847bb893fafbe0ae4081b
sabitathapa08 <strose07@gmail.com>
2019-10-22 18:02:39 +0545

    map added in different component

--- src/components/common/SiteMap.js
+++ src/components/common/SiteMap.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, createRef } from "react";
 //import { MDBDataTable } from 'mdbreact';
 import {
   Map,
@@ -6,50 +6,117 @@ import {
   LayersControl,
   Marker,
   Popup,
-  FeatureGroup,
-  Circle
+  GeoJSON
 } from "react-leaflet";
+import L, { latLngBounds } from "leaflet";
 // import "leaflet/dist/leaflet.css";
 
 import { BlockContentLoader } from "./Loader";
 import { markerIcon } from "./Marker";
 
+const { BaseLayer } = LayersControl;
 // const position = [27.7, 85.4];
-const MyMarkersList = data => {
-  const items = data.markers.map((props, key) => (
-    <MyPopupMarker key={key} {...props} />
-  ));
-  return <Fragment>{items}</Fragment>;
-};
 
-const MyPopupMarker = props => (
-  <Marker
-    position={[props.geometry.coordinates[1], props.geometry.coordinates[0]]}
-    icon={markerIcon}
-  >
-    <Popup>
-      <a href={props.url} target="_blank">
-        {props.properties.name}
-      </a>
-    </Popup>
-  </Marker>
-);
+// const MyMarkersList = data => {
+//   const items = data.markers.map((props, key) => (
+//     <MyPopupMarker key={key} {...props} />
+//   ));
+//   return <Fragment>{items}</Fragment>;
+// };
+
+// const MyPopupMarker = props => (
+//   <Marker
+//     position={[props.geometry.coordinates[1], props.geometry.coordinates[0]]}
+//     icon={markerIcon}
+//   >
+//     <Popup>
+//       <a href={props.url} target="_blank">
+//         {props.properties.name}
+//       </a>
+//     </Popup>
+//   </Marker>
+// );
 
 class SiteMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapData: props.map ? props.map : {},
+      bound: latLngBounds(
+        [29.38217507514529, 87.5390625],
+        [27.293689224852407, 81.474609375]
+      )
+    };
+    this.mapRef = createRef();
+    this.groupRef = createRef();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.map !== this.props.map) {
+      this.setState({
+        mapData: this.props.map
+      });
+    }
+  }
+  onEachFeaturePoint(feature, layer) {
+    layer.bindPopup(`<a href=${feature.url} target="_blank">
+    ${feature.properties.name}
+  </a>`);
+  }
+
+  pointToLayer(feature, latlng) {
+    const icon = new L.Icon({
+      iconUrl: require("../../static/images/marker.png"),
+      iconRetinaUrl: require("../../static/images/marker.png"),
+      iconSize: [28, 28],
+      iconAnchor: [13, 27],
+      popupAnchor: [2, -24],
+      shadowUrl: null,
+      shadowSize: null,
+      shadowAnchor: null
+      //iconSize: new L.Point(60, 75)
+      //className: "leaflet-div-icon"
+    });
+    return L.marker(latlng, { icon: icon });
+  }
+
   render() {
-    const { map, showContentLoader } = this.props;
+    const {
+      props: { showContentLoader },
+      state: { mapData }
+    } = this;
+
+    let bounds = latLngBounds();
+    mapData &&
+      mapData.features &&
+      mapData.features.forEach(data => {
+        bounds.extend([
+          data.geometry.coordinates[1],
+          data.geometry.coordinates[0]
+        ]);
+      });
 
     return (
       <>
         {showContentLoader ? (
           <BlockContentLoader number={1} height="395px" />
-        ) : !!map.features && map.features.length > 0 ? (
+        ) : !!mapData.features && mapData.features.length > 0 ? (
           <Map
             center={[
-              map.features[0].geometry.coordinates[1],
-              map.features[0].geometry.coordinates[0]
+              mapData.features[0].geometry.coordinates[1],
+              mapData.features[0].geometry.coordinates[0]
             ]}
-            zoom={13}
+            zoom={15}
+            maxZoom={20}
+            attributionControl={true}
+            zoomControl={true}
+            doubleClickZoom={true}
+            scrollWheelZoom={true}
+            dragging={true}
+            animate={true}
+            easeLinearity={0.35}
+            bounds={bounds}
+            ref={this.mapRef}
             style={{ width: "100%", height: "396px" }}
           >
             <TileLayer
@@ -57,20 +124,53 @@ class SiteMap extends Component {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <LayersControl position="topright">
-              <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+              <BaseLayer checked name="OpenStreetMap">
                 <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-                />
-              </LayersControl.BaseLayer>
-              <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
-                <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-              </LayersControl.BaseLayer>
+              </BaseLayer>
+              <BaseLayer name="Google Streets">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Hybrid">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Satellite">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Terrain">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
             </LayersControl>
-            <MyMarkersList markers={map.features} />
+            <GeoJSON
+              data={mapData}
+              onEachFeature={this.onEachFeaturePoint.bind(this)}
+              pointToLayer={this.pointToLayer.bind(this)}
+              ref={this.groupRef}
+            />
+
+            {/* <MyMarkersList markers={map.features} /> */}
             {/* {map.features.map((each, idx) => {
               const name = each.properties.name;
               const location = each.geometry.coordinates;
@@ -98,39 +198,45 @@ class SiteMap extends Component {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <LayersControl position="topright">
-              <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+              <BaseLayer checked name="OpenStreetMap">
                 <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-                />
-              </LayersControl.BaseLayer>
-              <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
-                <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-              </LayersControl.BaseLayer>
-
-              {/* <LayersControl.Overlay name="Feature group">
-                <FeatureGroup color="purple">
-                  <Popup>
-                    <span>Popup in FeatureGroup</span>
-                  </Popup>
-                  <Circle
-                    center={[coordinates[1], coordinates[0]]}
-                    radius={200}
-                  />
-                </FeatureGroup>
-              </LayersControl.Overlay> */}
+              </BaseLayer>
+              <BaseLayer name="Google Streets">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Hybrid">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Satellite">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
+              <BaseLayer name="Google Terrain">
+                <TileLayer
+                  attribution='&amp;copy <a href="http://maps.google.com">Google Maps</a> contributors'
+                  url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
+                  maxZoom={20}
+                  subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                />
+              </BaseLayer>
             </LayersControl>
-
-            {/* <Marker position={[coordinates[1], coordinates[0]]}>
-              <Popup>
-                <span>Name: {name}</span>
-                <br />
-                {address && <span>Address: {address}</span>}
-              </Popup>
-            </Marker> */}
           </Map>
         )}
       </>
