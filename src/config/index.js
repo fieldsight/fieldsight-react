35b26809d7761382a2d8bb83729da6effbb1169b
unknown <srthapa_611@yahoo.com>
2019-10-25 15:55:17 +0545

    fixed submission err

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-let csrfVal = window.csrf ? window.csrf : "FasSURCaozAKo7OHtkIhiTtOag7hVBBt";
-let tokenVal = window.token
-  ? window.token
-  : "91a844e62e86b6e336b8fb440340cbeaabf601fe";
-//"3f70407e649778bfa9a5d748e5b59e89d2f6ca44";
+// use your token in place of "xxxx-xxxx-xxxx-xxxx"
+// remove your token before pushing to github
+let tokenVal = window.token ? window.token : "xxxx-xxxx-xxxx-xxxx";
 
-//const url="91a844e62e86b6e336b8fb440340cbeaabf601fe"
 let baseURL = window.base_url
   ? window.base_url
   : "https://fieldsight.naxa.com.np/";
