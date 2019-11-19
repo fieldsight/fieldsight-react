03b422e3b9fa1dd6999dc209485fce2690f5d769
unknown <srthapa_611@yahoo.com>
2019-11-19 14:17:00 +0545

    changed axios headers

--- src/config/index.js
+++ src/config/index.js
@@ -6,12 +6,12 @@ let tokenVal = window.token ? window.token : process.env.TOKEN;
 let baseURL = window.base_url
   ? window.base_url
   : "https://fieldsight.naxa.com.np/";
-//: "https://app.fieldsight.org/";
 
 const setDefault = () => {
   axios.defaults.baseURL = baseURL;
-  axios.defaults.headers.common["X-CSRFTOKEN"] = csrfVal;
-  axios.defaults.headers.common["Authorization"] = tokenVal;
+  process.env.PLATFORM === "production"
+    ? (axios.defaults.headers.common["X-CSRFTOKEN"] = csrfVal)
+    : (axios.defaults.headers.common["Authorization"] = tokenVal);
 };
 
 export default setDefault;
