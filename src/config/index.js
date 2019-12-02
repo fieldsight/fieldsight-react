6d60767fcbfc469289cb9a29aec7b6c6d9ee75ca
12unishacharya <unisha.naxa@gmail.com>
2019-12-02 17:03:52 +0545

    minor issues fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 
-let csrfVal = window.csrf ? window.csrf : "FasSURCaozAKo7OHtkIhiTtOag7hVBBt";
-let tokenVal = window.token
-  ? window.token
-  : "19380d3ca2f98db10ffdb07e9a201f832c7153f9";
+const csrfVal = window.csrf ? window.csrf : process.env.CSRF;
+const tokenVal = window.token ? window.token : process.env.TOKEN;
 
-//const url="91a844e62e86b6e336b8fb440340cbeaabf601fe"
-let baseURL = window.base_url
+const baseURL = window.base_url
   ? window.base_url
-  : "http://192.168.1.44:8002/";
-  //"https://fieldsight.naxa.com.np/";
+  : // : " http://ec2-18-220-9-254.us-east-2.compute.amazonaws.com";
+    "https://fieldsight.naxa.com.np/";
+// "https://app.fieldsight.org/";
 
 const setDefault = () => {
   axios.defaults.baseURL = baseURL;
   axios.defaults.headers.common["X-CSRFTOKEN"] = csrfVal;
-  axios.defaults.headers.common["Authorization"] = tokenVal;
-  // axios.defaults.headers.common["Content-Type"] = "application/json";
+  axios.defaults.headers.common.Authorization = tokenVal;
 };
 
 export default setDefault;
