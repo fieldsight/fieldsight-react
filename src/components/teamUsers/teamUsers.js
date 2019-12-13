f69cfb38f2d4da657bd3f208563bd7fe8b306121
raeesaroj <raesaroj16@gmail.com>
2019-12-13 16:25:31 +0545

    remove user url of react.

--- src/components/teamUsers/teamUsers.js
+++ src/components/teamUsers/teamUsers.js
@@ -107,7 +107,7 @@ export default class TeamUser extends Component {
                       <tr key={key}>
                         <td>
                           <a
-                            href={`/fieldsight/application/#/profile/${users.id}`}
+                            href={`/users/profile/${users.id}`}
                             className="pending table-profile"
                           >
                             <figure>
